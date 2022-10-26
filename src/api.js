cb666059d2d51c077a6cb74fc0600235d8a8348f
Lucas C. Silveira <43255561+lucasyuk1@users.noreply.github.com>
2022-10-26 16:38:57 -0300

    adicionando segunda geração

--- src/api.js
+++ src/api.js
@@ -15,7 +15,7 @@ window.getPokeData = async function() {
 };
 
 async function getPokemon() {
-  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=251');
   const pokemon = await res.json();
   
   return pokemon.results;
